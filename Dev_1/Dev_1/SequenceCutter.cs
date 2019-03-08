3b6d0fa2cceac50d345d8572398723e6026af569
vladastr <vladastr21@gmail.com>
2019-03-09 00:29:16 +0300

    First

---
+++ Dev_1/Dev_1/SequenceCutter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev_1
+{
+    public class SequenceCutter
+    {
+        string[] resultsequences = { };
+
+        public static void Find(string s)
+        {
+            if (s.Length < 2)
+            {
+                Console.WriteLine("Less than 2 symbols!");
+                return;
+            }
+            for (int i = 0, j = 0; i < s.Length; i++)
+            {
+                if ((i + 1 < s.Length && s[i] == s[i + 1]) || (i == s.Length - 1))
+                {
+                    while (j != i)
+                    {
+                        int k = i - j;
+                        if (k >= 1)
+                        {
+                            while (k > 0)
+                            {
+                                Console.WriteLine(s.Substring(j, k + 1));
+                                k--;
+                            }
+                        }
+                        j++;
+                    }
+                    j++;
+                }
+            }
+        }
+        public void showResult()
+        {
+            for (int i = 0; i < resultsequences.Length - 1; i++)
+                Console.WriteLine(resultsequences[i]);
+        }
+    }
+}
